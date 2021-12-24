aa6c38560ce5531bb1ae9c4f2bcd96380b77665b
daegeon <kaelijin@naver.com>
2021-12-24 17:19:16 +0900

    한글번역 추가로 넣음

--- src/main/java/zzyzzy/HelloAgain.java
+++ src/main/java/zzyzzy/HelloAgain.java
@@ -3,8 +3,8 @@ package zzyzzy;
 public class HelloAgain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello, Again~!!");
+        System.out.println("안녕~ 돌아왔어~!!");
 	}
 
 }
